3cf62eee2693344e087670e91c1bd71ad4beced2
Robertson Bassy <robertson.bassy@gmail.com>
2024-03-11 10:47:23 -0400

    fixed carousel size

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,23 +3,13 @@ import Image from 'react-bootstrap/Image';
 import pic1 from '../assets/HomescreenGal/galPhoto0.JPG'
 import pic2 from '../assets/HomescreenGal/galPhoto1.JPG'
 import pic3 from '../assets/HomescreenGal/galPhoto2.JPG'
-import pic4 from '../assets/HomescreenGal/galPhoto3.JPG'
-import pic5 from '../assets/HomescreenGal/galPhoto4.JPG'
-import pic6 from '../assets/HomescreenGal/galPhoto5.JPG'
-import pic7 from '../assets/HomescreenGal/galPhoto6.JPG'
-import pic8 from '../assets/HomescreenGal/galPhoto7.JPG'
-import pic9 from '../assets/HomescreenGal/galPhoto8.JPG'
-import pic10 from '../assets/HomescreenGal/galPhoto9.JPG'
-import pic11 from '../assets/HomescreenGal/galPhoto10.JPG'
-import pic12 from '../assets/HomescreenGal/galPhoto11.JPG'
-import pic13 from '../assets/HomescreenGal/galPhoto12.JPG'
+
 import '../css/Carousel.css';
 
 function Carousels() {
   return (
     <Carousel style={{ height: '500px', display: 'flex', flexDirection: 'column',
-    justifyContent: 'space-around', alignItems: 'center', height: '600px',
-    textAlign: 'center' }}>
+    justifyContent: 'space-around', alignItems: 'center', textAlign: 'center' }}>
       <Carousel.Item>
       <Image src={pic1} alt="Iota Omega Chapter"  stretch/>
         <Carousel.Caption>
